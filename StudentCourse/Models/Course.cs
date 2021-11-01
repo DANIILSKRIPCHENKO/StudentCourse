86ac56bbf2936ce292dc84fcd79f46fd92932c0e
”Daniil <das76@tpu.ru>
2021-11-02 06:33:18 +0700

    Add project files.

---
+++ StudentCourse/Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentCourse.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Student> Students { get; set; } //виртуальное свойство 
+
+        public Course()
+        {
+            Students = new List<Student>();
+        }
+    }
+}
